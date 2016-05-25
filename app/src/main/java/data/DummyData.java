7cd695edf903243576acb4d770ff70ab641e614f
imariamj <mariam.javed@flipkart.com>
2016-05-25 19:29:14 +0530

    Added data

--- app/src/main/java/data/DummyData.java
+++ app/src/main/java/data/DummyData.java
@@ -198,7 +198,150 @@ public class DummyData {
             options20.add(new Option(2, "Dilip Kumar"));
             options20.add(new Option(3, "Raj Kapoor"));
             options20.add(new Option(4, "Kishore Kumar"));
-            QUESTION.add(new Question(20,"Beautiful actress Madhubala was married to?", getCategory(1), options20, options20.get(3)));}
+            QUESTION.add(new Question(20,"Beautiful actress Madhubala was married to?", getCategory(1), options20, options20.get(3)));
+
+
+            List<Option> options21 = new ArrayList<Option>();
+            options21.add(new Option(1, "Sher Khan"));
+            options21.add(new Option(2, "Mufasa"));
+            options21.add(new Option(3, "Simba"));
+            options21.add(new Option(4, "Sarabi"));
+            QUESTION.add(new Question(21,"What is the name of the young lion whose story is told in the musical 'The Lion King'?", getCategory(2), options21, options21.get(2)));
+
+            List<Option> options22 = new ArrayList<Option>();
+            options22.add(new Option(1, "Scotland"));
+            options22.add(new Option(2, "England"));
+            options22.add(new Option(3, "Germany"));
+            options22.add(new Option(4, "Netherland"));
+            QUESTION.add(new Question(22,"Which country's freedom struggle is portrayed in the Mel Gibson movie 'Braveheart'?", getCategory(2), options22, options22.get(3)));
+
+            List<Option> options23 = new ArrayList<Option>();
+            options23.add(new Option(1, "Letters to Juliet"));
+            options23.add(new Option(2, "Saving Private Ryan"));
+            options23.add(new Option(3, "Forest Gump"));
+            options23.add(new Option(4, "Gone With The Wind"));
+            QUESTION.add(new Question(23,"Which movie had this dialogue \"My mama always said, life was like a box of chocolates. You never know what you're gonna get.", getCategory(2), options23, options23.get(2)));
+
+            List<Option> options24 = new ArrayList<Option>();
+            options24.add(new Option(1, "Die"));
+            options24.add(new Option(2, "Golden"));
+            options24.add(new Option(3, "Casino"));
+            options24.add(new Option(4, "Never"));
+            QUESTION.add(new Question(24,"Excluding \"the\", which word appears most often in Bond movie titles?", getCategory(2), options24, options24.get(3)));
+
+            List<Option> options25 = new ArrayList<Option>();
+            options25.add(new Option(1, "Bishop"));
+            options25.add(new Option(2, "Ash"));
+            options25.add(new Option(3, "Call"));
+            options25.add(new Option(4, "Carlos"));
+            QUESTION.add(new Question(25,"Name the android in movie Alien ", getCategory(2), options25, options25.get(1)));
+
+            List<Option> options26 = new ArrayList<Option>();
+            options26.add(new Option(1, "Gone with the wind"));
+            options26.add(new Option(2, "Home footage"));
+            options26.add(new Option(3, "With Our King and Queen Through India"));
+            options26.add(new Option(4, "Treasure Island"));
+            QUESTION.add(new Question(26,"Which was the first colour film to win a Best Picture Oscar?", getCategory(2), options26, options26.get(0)));
+
+            List<Option> options27 = new ArrayList<Option>();
+            options27.add(new Option(1, "Robert Redford"));
+            options27.add(new Option(2, "Michael Douglas"));
+            options27.add(new Option(3, "Harrison Ford"));
+            options27.add(new Option(4, "Patrick Swayze"));
+            QUESTION.add(new Question(27,"Who played the male lead opposite Sharon Stone in the hugely successful movie 'The Basic Instinct'?", getCategory(2), options27, options27.get(1)));
+
+            List<Option> options28 = new ArrayList<Option>();
+            options28.add(new Option(1, "10,000 BC"));
+            options28.add(new Option(2, "Day after tomorrow"));
+            options28.add(new Option(3, "2012"));
+            options28.add(new Option(4, "The Noah's Ark Principle"));
+            QUESTION.add(new Question(28,"Which Roland Emmerich movie portrays fictional cataclysmic events that were to take place in early 21st century?", getCategory(2), options28, options28.get(2)));
+
+            List<Option> options29 = new ArrayList<Option>();
+            options29.add(new Option(1, "Finding Nemo"));
+            options29.add(new Option(2, "The Incredibles"));
+            options29.add(new Option(3, "Monsters, Inc."));
+            options29.add(new Option(4, "Toy Story"));
+            QUESTION.add(new Question(29,"What was the first movie by Pixar to receive a rating higher than G in the United States?", getCategory(2), options29, options29.get(1)));
+
+            List<Option> options30 = new ArrayList<Option>();
+            options30.add(new Option(1, "Draco"));
+            options30.add(new Option(2, "Harry"));
+            options30.add(new Option(3, "Hermione"));
+            options30.add(new Option(4, "Ron"));
+            QUESTION.add(new Question(30,"In the 'Prisoner of Azkaban', who throws rocks at Hagrid's hut so that Harry, Ron and Hermione can leave?", getCategory(2), options30, options30.get(2)));
+
+            List<Option> options31 = new ArrayList<Option>();
+            options31.add(new Option(1, "Brett Lee"));
+            options31.add(new Option(2, "Adam Gilchrist"));
+            options31.add(new Option(3, "Jason Gillespie"));
+            options31.add(new Option(4, "Glenn McGrath"));
+            QUESTION.add(new Question(31,"'Dizzy' is the nickname of what Australian player?", getCategory(3), options31, options31.get(2)));
+
+            List<Option> options32 = new ArrayList<Option>();
+            options32.add(new Option(1, "1883 between Australia and Wales"));
+            options32.add(new Option(2, "1844 between Canada and the USA"));
+            options32.add(new Option(3, "1869 between England and Australia"));
+            options32.add(new Option(4, "1892 between England and India"));
+            QUESTION.add(new Question(32,"In what year was the first international cricket match held?", getCategory(3), options32, options32.get(1)));
+
+            List<Option> options33 = new ArrayList<Option>();
+            options33.add(new Option(1, "Salim Durrani"));
+            options33.add(new Option(2, "Farooq Engineer"));
+            options33.add(new Option(3, "Vijay Hazare"));
+            options33.add(new Option(4, "Mansur Ali Khan Pataudi"));
+            QUESTION.add(new Question(33,"Which former Indian cricketer was nicknamed 'Tiger'?", getCategory(3), options33, options33.get(3)));
+
+            List<Option> options34 = new ArrayList<Option>();
+            options34.add(new Option(1, "Piyush Chawla"));
+            options34.add(new Option(2, "Gautam Gambhir"));
+            options34.add(new Option(3, "Irfan Pathan"));
+            options34.add(new Option(4, "Suresh Raina"));
+            QUESTION.add(new Question(34,"Who was named as the ICC Emerging Player of the Year 2004?", getCategory(3), options34, options34.get(2)));
+
+            List<Option> options35 = new ArrayList<Option>();
+            options35.add(new Option(1, "Subhash Gupte"));
+            options35.add(new Option(2, "M.L.Jaisimha"));
+            options35.add(new Option(3, "Raman Lamba"));
+            options35.add(new Option(4, "Lala Amarnath"));
+            QUESTION.add(new Question(35,"Which cricketer died on the field in Bangladesh while playing for Abahani Club?", getCategory(3), options35, options35.get(2)));
+
+            List<Option> options36 = new ArrayList<Option>();
+            options36.add(new Option(1, "V. Raju"));
+            options36.add(new Option(2, "Rajesh Chauhan"));
+            options36.add(new Option(3, "Arshad Ayub"));
+            options36.add(new Option(4, "Narendra Hirwani"));
+            QUESTION.add(new Question(36,"In 1987-88, which Indian spinner took 16 wickets on his Test debut?", getCategory(3), options36, options36.get(3)));
+
+            List<Option> options37 = new ArrayList<Option>();
+            options37.add(new Option(1, "Ravi Shastri"));
+            options37.add(new Option(2, "Dilip Vengsarkar"));
+            options37.add(new Option(3, "Sachin Tendulkar"));
+            options37.add(new Option(4, "Sanjay Manjrekar"));
+            QUESTION.add(new Question(37,"Which former Indian Test cricketer equalled Gary Sobers world record of six sixes in an over in a Ranji Trophy match?", getCategory(3), options37, options37.get(0)));
+
+            List<Option> options38 = new ArrayList<Option>();
+            options38.add(new Option(1, "Mohsin khan"));
+            options38.add(new Option(2, "Wasim Akram"));
+            options38.add(new Option(3, "Naveen Nischol"));
+            options38.add(new Option(4, "None Of The Above"));
+            QUESTION.add(new Question(38,"Reena Roy the Indian film actress married a cricketer?", getCategory(3), options38, options38.get(0)));
+
+            List<Option> options39 = new ArrayList<Option>();
+            options39.add(new Option(1, "London, England"));
+            options39.add(new Option(2, "Mumbai, India"));
+            options39.add(new Option(3, "Lahore, Pakistan"));
+            options39.add(new Option(4, "Nairobi, Kenya"));
+            QUESTION.add(new Question(39,"Where did Yuvraj Singh make his ODI debut?", getCategory(3), options39, options39.get(3)));
+
+            List<Option> options40 = new ArrayList<Option>();
+            options40.add(new Option(1, "Arvind De Silva"));
+            options40.add(new Option(2, "Roshan Mahanama"));
+            options40.add(new Option(3, "Harshan Tilakaratne"));
+            options40.add(new Option(4, "None Of The Above"));
+            QUESTION.add(new Question(40,"Kapil Dev's 432 (world) record breaking victim was?", getCategory(3), options40, options40.get(2)));
+
+        }
         return QUESTION;
     }
 
